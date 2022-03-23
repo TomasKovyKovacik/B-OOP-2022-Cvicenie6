52f06d64f23c98aae417dec51c09290689eca8b6
TomasKovyKovacik <tomas_kovacik@stuba.sk>
2022-03-23 15:31:25 +0100

    zaklad enum

--- src/main/java/sk/stuba/fei/uim/oop/Main.java
+++ src/main/java/sk/stuba/fei/uim/oop/Main.java
@@ -2,6 +2,22 @@ package sk.stuba.fei.uim.oop;
 
 public class Main {
     public static void main(String[] args) {
+        SvetoveStrany strana = SvetoveStrany.JUH;
 
+        for (SvetoveStrany s :SvetoveStrany.values()) {
+            checkSever(s);
+        }
+
+        checkSever(SvetoveStrany.valueOf("SEVER"));
+        checkSever(strana);
+        checkSever(SvetoveStrany.valueOf("sever"));
+    }
+
+    public static void checkSever(SvetoveStrany s) {
+        if (s.equals(SvetoveStrany.SEVER)) {
+            System.out.println("je sever");
+        } else {
+            System.out.println("nie je sever");
+        }
     }
 }
---
+++ src/main/java/sk/stuba/fei/uim/oop/SvetoveStrany.java
@@ -0,0 +1,8 @@
+package sk.stuba.fei.uim.oop;
+
+public enum SvetoveStrany {
+    SEVER,
+    JUH,
+    VYCHOD,
+    ZAPAD;
+}
